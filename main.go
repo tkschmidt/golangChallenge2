df9080e1a74e152e8a6f97603c01b436eee749c6
Tobias Schmidt <tobias@tkschmidt.me>
2015-04-15 10:34:19 +0200

    embedding works now golint shows no errors also 3/5 tests deliver no error

--- main.go
+++ main.go
@@ -11,17 +11,19 @@ import (
 	"os"
 )
 
+// SecureReader is bla
+// needs comments because its exported (because uppercase)
 type SecureReader struct {
-	r    io.Reader
+	io.Reader
 	priv *[32]byte
 	pub  *[32]byte //new marker if multiple reads are necessary
 }
 
 func (sr *SecureReader) Read(p []byte) (n int, err error) {
-	n, r := sr.r.Read(p)
-	nonce_back := new([24]byte)
-	copy(nonce_back[:], p[:24])
-	decrypt, _ := box.Open(nil, p[24:n], nonce_back, sr.pub, sr.priv)
+	n, r := sr.Reader.Read(p)
+	nonceBack := new([24]byte)
+	copy(nonceBack[:], p[:24])
+	decrypt, _ := box.Open(nil, p[24:n], nonceBack, sr.pub, sr.priv)
 	buf := make([]byte, 1024)
 	copy(p, buf)
 	copy(p[:n], decrypt)
@@ -36,8 +38,9 @@ func NewSecureReader(r io.Reader, priv, pub *[32]byte) io.Reader {
 	return nsr
 }
 
+// SecureWriter needs also a comment
 type SecureWriter struct {
-	w    io.Writer
+	io.Writer
 	priv *[32]byte
 	pub  *[32]byte
 }
@@ -49,13 +52,13 @@ func (sw *SecureWriter) Write(p []byte) (n int, err error) {
 		panic(err)
 	}
 	enc := box.Seal(nil, p, nonce, sw.pub, sw.priv)
-	enc_with_nonce := append(nonce[:], enc...)
-	n, err = sw.w.Write(enc_with_nonce)
+	encWithNonce := append(nonce[:], enc...)
+	n, err = sw.Writer.Write(encWithNonce)
 	return n, err
 }
 
-// https://github.com/mperham/gobox
 // NewSecureWriter instantiates a new SecureWriter
+// https://github.com/mperham/gobox
 func NewSecureWriter(w io.Writer, priv, pub *[32]byte) io.Writer {
 	nsw := &SecureWriter{w, priv, pub}
 	nsw.priv = priv
@@ -63,18 +66,47 @@ func NewSecureWriter(w io.Writer, priv, pub *[32]byte) io.Writer {
 	return nsw
 }
 
+type secureReadWriteCloser struct {
+	io.Writer
+	io.Reader
+}
+
+func (srwc secureReadWriteCloser) Close() (err error) {
+	return nil
+}
+
 // Dial generates a private/public key pair,
 // connects to the server, perform the handshake
 // and return a reader/writer.
 func Dial(addr string) (io.ReadWriteCloser, error) {
-	return nil, nil
+	pub, priv, err := box.GenerateKey(rand.Reader)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+	secureR := NewSecureReader(conn, priv, pub)
+	secureW := NewSecureWriter(conn, priv, pub)
+
+	var rwc secureReadWriteCloser
+	rwc.Writer = secureW
+	rwc.Reader = secureR
+
+	return rwc, err
 }
 
 // Serve starts a secure echo server on the given listener.
 func Serve(l net.Listener) error {
-	return nil
-}
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Printf("i have an error %v\n", err)
+			panic(err)
+		}
+		go io.Copy(c, c)
+		return nil
 
+	}
+}
 func main() {
 	port := flag.Int("l", 0, "Listen mode. Specify port")
 	flag.Parse()
